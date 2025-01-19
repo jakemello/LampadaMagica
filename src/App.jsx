808781647ae9ed90ea5b7144057796df1b649f6c
jakemello <jakelinemello@live.com>
2025-01-19 17:35:35 -0300

    Desafio Gênio da Lâmpada

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import lampadaMagica from './assets/lampadaMagica.png'
+import genioDaLampada from './assets/genioDaLampada.png'
+import { useState } from 'react'
+import './App.scss'
+
+
+export default function App(){
+
+// Váriavel de estado
+// Uma caixinha que guarda um valor que pode mudar
+// Quando o usuário interagir com a página
+
+// Variável [valor atual, função que guarda quando o valor for atualizado]
+
+ const [lampada, setLampada] = useState(lampadaMagica) // Se fosse usar ' ' seria apenas para tipos de texto, e não iria renderizar a imagem na tag img
+ const mudarLampada = () => {
+   setLampada(lampada === lampadaMagica ? genioDaLampada : lampadaMagica)
+ }
+
+// Arrow Function
+// variavél nomeDaVariavel = arrowFunction {
+// lógica/código que será executado}
+
+  return (
+    <main>
+      <h1>"Liberte a magia da programação com a lâmpada de Aladim."</h1>
+      <section className={`balaoFala ${lampada === genioDaLampada ? 'visivel' : ''}`}>
+        <p>"Olá! Eu sou o gênio da lâmpada! E vou te conceder 3 desejos!".</p>
+      </section>
+      <img src={lampada} alt="" />
+      <button onClick={mudarLampada}>Clique Aqui</button>
+    </main>
+  )
+}
